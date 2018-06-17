8c2bd720ac6c02f7bea594134ccf0fb9bb45d9fa
NILESH GAONKAR <nileshgaonkar19@gmail.com>
2018-06-17 23:15:34 +0530

    added by nilesh

---
+++ a.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>parameter is number - Clean URL with .htaccess and PHP by http://codeofaninja.com/</title>
+         
+        </style>
+    </head>
+<body>
+ 
+<h1>Parameter: Number ONLY. This is parameter_number.php</h1>
+<?php echo "PARAMETER VALUE: " . $_GET['profID']; ?>
+ 
+</body>
+</html>
